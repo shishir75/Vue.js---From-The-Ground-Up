da2e25a7ccc3739ab840bbff961e4bc662ac444a
shishir75 <shishir.srdr16@gmail.com>
2020-02-06 03:00:48 +0600

    Custom Events Passing Data from Child to Parent Component

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import Vue from 'vue';
 import Accordion from "./components/Accordion";
+import List from "./components/List";
 
 
 new Vue({
     el: '#app',
     components: {
-        Accordion,
+        Accordion, List,
     },
     data: {
         items: [
---
+++ src/components/List.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p>ITEM</p>
+
+        <dropdown></dropdown>
+
+    </div>
+</template>
+
+<script>
+    import Dropdown from "./Dropdown";
+
+    export default {
+        name: "List",
+
+        components: {
+            Dropdown,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
